9c306458dd2abbecd65e00dde4b71b1ce0021d63
noah <noah.u22@gmail.com>
2022-11-12 12:31:51 -0500

    commit

--- projects/software/thissite.js
+++ projects/software/thissite.js
@@ -1,7 +1,7 @@
 const thissite = {
     title:"My Personal Page (yes this one)🕸️",
     descriptionShort:"Building my personal portfolio page with Next.js and taking half the time to build a stupid scrollspy",
-    descriptionFull:"I'm scared of the internet because my dad told me I'd get molested when I was a kid. Also like most millenials I got aboslutely fried in call-of-duty lobbies for having a high-pitched voice and after that I vowed never to reveal my true identity online. A few years later, and still with a voice that I wish was a couple octaves lower, here I am realizing I need to be able to put myself out there more. I wanted to build a cool modular site with React but I knew I'd get zingered by the senior devs for using a heavy framework for a static site so, at the behest of my boy Johnny Pribyl, I scooped up next.js and here we are. I hope you like it, feel free to fork it and use it!",
+    descriptionFull:"I'm scared of the internet because when I was a kid my dad told me everybody I met on world of warcraft was probably a molester. Also like most millenials I got aboslutely fried in call-of-duty lobbies for having a high-pitched voice and after that I vowed never to reveal my true identity online. A few years later, and still with a voice that I wish was a couple octaves lower, here I am realizing I need to be able to put myself out there more. I wanted to build a cool modular site with React but I knew I'd get zingered by the senior devs for using a heavy framework for a static site so, at the behest of my boy Johnny Pribyl, I scooped up next.js and here we are. I hope you like it, feel free to fork it and use it!",
     links:["https://github.com/nerr22/noahu"],
     image:''
 }
