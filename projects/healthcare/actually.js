d226abc6b4a08c922469c1fcffc9d7016313c274
noah <noah.u22@gmail.com>
2022-11-13 16:21:37 -0500

    content update

--- projects/healthcare/actually.js
+++ projects/healthcare/actually.js
@@ -2,7 +2,7 @@
 const actually = {
     title:"Actually Health🏥",
     descriptionShort:"My Vision of Health 3.0: Using heuristic lableing philosophy to augment patients in generating high value RWE. Value so high their care is free $$$$",
-    descriptionFull:"Companies like flatiron, iqvia, and nference have a fundamental flaw. They ignore supply chain contstrains on their data. Everybody talks about how 'big data' is going to fix healthcare as if 'big data' just exists and we can use it. They don't realize big data needs to be transated from objective and subjective human experience into data first. That is the rate limiting step and that is why I founded Actually. Our engine is powered by an ensemble of biomedical and clinical knowledge graphs that we couple with a data-focused UI to help patients generate clinically relevant insights.",
+    descriptionFull:"'RWE' companies like iqvia and flatiron have a fundamental flaw. They ignore supply chain contstrains on their data. Everybody talks about how 'big data' is going to fix healthcare as if 'big data' is both passively generated, and inherently structured in a useful manner. Little, if any, thought is given to the complexities of taking subjective human experiences and making them computable at scale. That is the rate limiting step and that is why I founded Actually. Our engine is powered by an ensemble of biomedical and clinical knowledge graphs that we couple with a data-focused UI to help patients generate clinically relevant insights. With a scalable automated phenoytping platform like ours, critical health infrastructure research like pharmacogenomics is as easy as set it and forget it. At least, thats the plan.",
     links:['https://actually.health','https://github.com/liquet-ai'],
     images:[]
 }
--- projects/healthcare/generics.js
+++ projects/healthcare/generics.js
@@ -1,7 +1,7 @@
 const generics = {
     title:"Affordable Generics💊",
     descriptionShort:"Scraping drug API prices from wholesalers and quantifying the descrepancies between pricing and cost",
-    descriptionFull:"I became interested in the delta between drug price, and cost after researching pharma companies Valeant, Turing, and Retrophin. I spent a few weeks reading SEC filings and literature. Using my experience in drug development, specifically pricing contract manufacturing, I designed a cost-based pricing model that accounted for manufacturing and regulatory prices in order to quantify the true cost-based price of drug production and compare it to the prices being set by insurers. ",
+    descriptionFull:"I became interested in the delta between drug price and cost after researching pharma companies Valeant, Turing, and Retrophin. I spent a few weeks reading SEC filings and literature. Then,sing my experience in drug development, specifically pricing contract manufacturing, I designed a cost-based pricing model that accounted for manufacturing and regulatory costs in order to quantify the true cost-based price of end-to-end drug production and compare it to the prices being set by insurers. ",
     links:[]
 }
 export default generics
